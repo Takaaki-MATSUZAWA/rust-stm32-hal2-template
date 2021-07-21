bc52d40f8b9fa438a9a2a4a300c72da4861c77b8
Takaaki-MATSUZAWA <matsuzawa@furo.org>
2021-07-21 11:19:46 +0900

    F446用に修正

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() -> ! {
     let clock_cfg = Clocks::default();
     clock_cfg.setup(&mut dp.RCC, &mut dp.FLASH).unwrap();
 
-    let mut led = Pin::new(Port::B, 8, PinMode::Output);
+    let mut led = Pin::new(Port::A, 5, PinMode::Output);
 
     let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
     
