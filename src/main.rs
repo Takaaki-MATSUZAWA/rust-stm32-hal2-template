e8a0a3766a012910868f1215ff7732eddb5e0368
Takaaki-MATSUZAWA <matsuzawa@furo.org>
2021-07-21 11:14:42 +0900

    クレートのバージョンを更新

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::{self, asm::delay, delay, prelude::_embedded_hal_blocking_delay_DelayMs};
+use cortex_m::{
+    self,
+    delay::Delay,
+};
 use cortex_m_rt::entry;
 use panic_halt as _;
 
 use stm32_hal2::{
     clocks::Clocks,
-    gpio::{GpioB, PinMode, OutputType},
-    low_power,
+    gpio::{Pin, Port, PinMode},
     pac,
-    timer::{Timer, TimerInterrupt},
 };
 
 #[entry]
 fn main() -> ! {
-    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
     let mut dp = pac::Peripherals::take().unwrap();
 
     let clock_cfg = Clocks::default();
     clock_cfg.setup(&mut dp.RCC, &mut dp.FLASH).unwrap();
 
-    let mut gpiob = GpioB::new(dp.GPIOB, &mut dp.RCC);
-    let mut led = gpiob.new_pin(8, PinMode::Output);
+    let mut led = Pin::new(Port::B, 8, PinMode::Output);
+
+    let mut delay = Delay::new(cp.SYST, clock_cfg.systick());
     
     let mut count = 0;
-
+    
     loop {
         if count%2 == 1 {
             led.set_high();
@@ -34,6 +36,7 @@ fn main() -> ! {
         }
 
         count += 1;
-        delay(170_000_000);
+        delay.delay_ms(500);
+
     }
 }
